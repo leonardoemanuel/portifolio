1ef1b9587f272fb00d5cbff5d2a83584687aacce
Leonardo Emanuel <leonardodev2005@gmail.com>
2022-12-20 13:19:16 -0300

    sistema de idade

--- script.js
+++ script.js
@@ -3,7 +3,11 @@ let ano = new Date()
 let anoAtual=ano.getFullYear()
 let mes=new Date()
 let mesAtual=mes.getMonth()
-if(mesAtual==10){
-    idade.innerText=`${anoAtual-2005}`
+if(mesAtual == 10 || mesAtual==11){
+    idade.innerHTML=anoAtual - 2005
+}else{
+    idade.innerHTML=(anoAtual -2005)-1
 }
+
+
 
